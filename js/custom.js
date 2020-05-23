7d4c049ac5f6fdb12120978b7711643d3d3fa258
baaae <reecehudson10@gmail.com>
2020-05-23 17:45:44 -0500

    likes backend, raw implementation

--- js/custom.js
+++ js/custom.js
@@ -178,3 +178,51 @@ function DoJsonStuffToShowHeartNumbers() {
         $("#num-hearts").text(data.hearts)
     })
 }
+
+// TODO: authenticate!!!
+AWS.config.region = 'us-west-1'
+AWS.config.credentials = new AWS.Credentials({
+    accessKeyId: "AKIAZDBDBAHOLE5TGKGF",
+    secretAccessKey: "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"
+});
+
+const bucket = new AWS.S3({
+    params: {
+        Bucket: "fight-rona",
+        Key: "click-count.json"
+    }
+});
+
+/**
+ * The likes feature works like this, the user clicks the likes btn repeatedly
+ * and as many times as he wants. We need to tell the server how many clicks to
+ * add to the count.
+ *
+ * Example, if the count was previously 47 and the user clicked 3 times, we
+ * tell the server to add 3 to the count.
+ *
+ * Ideally, we would wait a certain amount of time before updating the server
+ * with the extra clicks to add. Why? Because we assume that the user will tap/click
+ * on the like button pretty fast and we don't want to send a request for every click.
+ * Instead we should wait a certain a mount of time (I say 3 seconds) when the
+ * click button is idle -- meanin it hasn't been clicked for at least 3 seconds --
+ * and then update the server with the extra counts to add.
+ */
+function addToClickCount(extraClicks) {
+  // ...
+}
+
+async function getClickCount() {
+  let data
+  // https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/using-promises.html
+  try {
+    data = await bucket.getObject().promise()
+  } catch(err) {
+    console.log("Failed to fetch clicks", err)
+    return
+  }
+
+  const count =  JSON.parse(data.Body.toString("utf-8")).count
+  console.log("getClickCount() count =>", count")
+  return +count
+}
