9f1cc5153e395d94b582925031c2832f5074ac20
JacobHornbeck <hor20009@byui.edu>
2020-07-05 00:05:47 -0600

    I still am going to change a bit more

--- js/social-collage.js
+++ js/social-collage.js
@@ -1,25 +1,81 @@
-let item_sizes = [
-    'grid-medium',
-    'grid-small',
-    'grid-small',
-    'grid-large',
-    'grid-medium',
-    'grid-large',
-    'grid-medium',
-    'grid-small',
-    'grid-large',
-    'grid-medium',
-    'grid-medium',
-    'grid-small',
+
+/** The array above we may substitute with a jquery/ajax/http
+ *  request. It may be easier to use the array... especially
+ *  if we are going to do the ChangePosts() function. **/
+var some_social_posts = [
+    /** Example: **/
+    /*{
+        // Any image really, it could be the posters profile pic or
+        // it could be some image that they put in the post
+        'image-url': 'https://www.dummyimage.com/50x50',
+        // This can replace 'post-title' if we don't have the title
+        'user-name': 'Someone',
+        'post-title': 'Some Title', // Optional
+        'post-content': 'filler text, filler text, filler text',
+        // We can send them to the post if they want
+        'post-url': 'https://www.facebook.com/somepost'
+    }*/
+
 ]
 
-var grid_collage_interval = 'somethingtochange'
-function SetCollageGrid() {
-    if ($('.social-grid-item').length>0) {
-        $('.social-grid-item').each(function(index) {
-            $(this).addClass(item_sizes[index])
-        })
-        clearInterval(grid_collage_interval)
+
+if ( some_social_posts.length > 10 ) {
+    /**  **/
+    const posts_start = Math.floor( Math.random() * ( some_social_posts.length - 0.1 ) )
+    const posts_end = ( posts_start + 10 ) % ( some_social_posts.length - 1 )
+}
+else {
+    /*alert("ERROR: Posts under limit")*/
+}
+
+
+/** This function will be called once the first post element
+ *  loads. That way we know that it isn't trying to change
+ *  text and images of things that don't exist. **/
+function ShowPosts() {
+    
+}
+
+
+/** This function will be called ever-so-often to make
+ *  some of the posts change. We don't have to include
+ *  this, but it would be cool. I was thinking maybe
+ *  have it when they change it flips around and then
+ *  shows a different post. **/
+function ChangePosts() {
+    
+}
+
+
+/** This function will allow the user to click the images
+ *  on the post and it will enlage it (or so it will seem) **/
+function EnlargePic(elem) {
+    let mouse = {
+        'x': event.clientX,
+        'y': event.clientY
+    }
+    let a = document.getElementById('img-show')
+    let b = document.getElementById('partial-color')
+    a.src = elem.src
+    a.style.transition = 'all 0s linear'
+    a.style.top = (mouse.y-(256/2))+'px'
+    a.style.left = (mouse.x-(256/2))+'px'
+    a.style.transition = 'all 0s linear'
+    a.className = 'large'
+    a.style.top = 'calc(50% - 128px)'
+    a.style.left = 'calc(50% - 128px)'
+    a.style.transition = 'all 0.5s cubic-bezier(0.68, -0.55, 0.265, 1.55)'
+    b.style.background = 'rgba(0,0,0,0.5)'
+}
+
+let some_iterator = ''
+function LoadListeners() {
+    if (document.getElementsByClassName('show-img').length > 0) {
+        let _this = document.getElementsByClassName('show-img')
+        for (let i = 0; i < _this.length; i++) {
+            _this[i].addEventListener('click', () => {EnlargePic(_this[i])})
+        }
+        clearInterval(some_iterator)
     }
 }
-grid_collage_interval = setInterval(SetCollageGrid, 500);
+some_iterator = setInterval(LoadListeners, 100);
